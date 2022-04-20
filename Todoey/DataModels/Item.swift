70c582cfa2b8272a8db37d306d66df9cefb0bbcc
Sizwe Khathi <sizwek@globalkinetic.com>
2022-04-20 02:04:19 +0200

    Refactor the code by introducing Item class
    This is done to resolve the checkmark issue that occurs when the user scrolls the tableView out of view

---
+++ Todoey/DataModels/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Toodooey
+//
+//  Created by Sizwe Khathi on 2022/04/20.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
--- Todoey/TodoListVC.swift
+++ Todoey/TodoListVC.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class TodoListVC: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Defeat Demogorgon"]
+    var itemArray = [Item]()
     let userDefaults = UserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem1 = Item()
+        newItem1.title = "Buy Eggos"
+        newItem1.done = false
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Destroy Demogorgon"
+        newItem2.done = false
+        itemArray.append(newItem2)
     }
     
     //MARK: - Tableview Datasource Methods
@@ -28,21 +43,36 @@ class TodoListVC: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = item
+        cell.textLabel?.text = item.title
+        
+        //MARK:- Ternary Operator
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        //The above is same as the following
+//        if item.done == true{
+//            cell.accessoryType = .checkmark
+//        }else{
+//            cell.accessoryType = .none
+//        }
         return cell
     }
     
     //MARK: - Table View Delegate and methos
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        //print(itemArray[indexPath.row])
-        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark) {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        //The above is same as the following
+        //        if itemArray[indexPath.row].done == false {
+        //            titemArray[indexPath.row].done == true
+        //        } else {
+        //            itemArray[indexPath.row].done == false
+        //        }
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -53,8 +83,10 @@ class TodoListVC: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { [self] (action) in
             //the action once the user clicks add item
+            let addItem = Item()
+            addItem.title = textField.text!
             
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(addItem)
             
             self.tableView.reloadData()
             //reload tableView to show the data once more
