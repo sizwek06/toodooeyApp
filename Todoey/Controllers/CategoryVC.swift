31b646f255184f51cd549b18c0bea60de47ab2f9
Sizwe Khathi <sizwek@globalkinetic.com>
2022-04-23 05:18:38 +0200

    Add categories items in views, controllers and core data

---
+++ Todoey/Controllers/CategoryVC.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryVC.swift
+//  Toodooey
+//
+//  Created by Sizwe Khathi on 2022/04/23.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+
+}
